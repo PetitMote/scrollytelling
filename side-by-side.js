d6e6d971da03ff9b45f544133c9fb849f6a05639
Moté <automates@lepouete.fr>
2023-06-24 16:48:49 +0200

    Initial commit

---
+++ side-by-side.js
@@ -0,0 +1,37 @@
+let map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+document.addEventListener('scroll-scene-enter', (event) => {
+    const step = event.detail.element.getAttribute('step');
+    event.detail.element.classList.add('is-active');
+    switch (step) {
+        case '1':
+            map.setView([51.505, -0.09], 13);
+            break;
+        case '2':
+            map.setView([51.505, -0.09], 10);
+            break;
+        case '3':
+            map.setView([51.505, -0.09], 5);
+            break;
+        case 'hide map':
+            document.querySelector('#map').setAttribute('hidden', 'true')
+            break;
+    }
+})
+
+document.addEventListener('scroll-scene-exit', (event) => {
+    const step = event.detail.element.getAttribute('step');
+    event.detail.element.classList.remove('is-active');
+    switch (step) {
+        case 'hide map':
+            if (!event.detail.isScrollingDown) {
+                document.querySelector('#map').removeAttribute('hidden')
+            }
+            break;
+    }
+})
