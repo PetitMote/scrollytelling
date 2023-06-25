d16cceb744a1b2fecd738aa91a7d5b86562e40c8
Moté <automates@lepouete.fr>
2023-06-25 19:24:01 +0200

    Almost working

--- side-by-side.js
+++ side-by-side.js
@@ -6,29 +6,51 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 
-const ctx = document.getElementById('chart');
+const chartSupFrance = document.getElementById('chart-superficie-france').querySelector('canvas');
 
-new Chart(ctx, {
+new Chart(chartSupFrance, {
     type: 'pie',
     data: {
         labels: ['Aucun habitant', 'Présence d’habitants'],
         datasets: [{
-            label: 'Pourcentage de la surface',
-            data: [40, 60],
+            label: 'Surface (ha)',
+            data: [17500000, 37500000],
             borderWidth: 1
         }]
     }
 })
 
+function show_figure (id) {
+    const element = document.querySelector(`#${id}`);
+    element.style.visibility = 'visible';
+    element.style.opacity = '100%';
+}
+
+function hide_figure (id) {
+    const element = document.querySelector(`#${id}`);
+    if (element.style.visibility === 'visible') {
+        element.addEventListener("transitionend", () => {
+            element.style.visibility = 'collapse';
+        }, {once: true});
+        element.style.opacity = '0';
+    }
+}
+
 document.addEventListener('scroll-scene-enter', (event) => {
     const step = event.detail.element.getAttribute('step');
     event.detail.element.classList.add('is-active');
     switch (step) {
-        case '1':
-            map.setView([51.505, -0.09], 13);
+        case 'hello':
+            show_figure('france');
             break;
-        case '2':
-            map.setView([51.505, -0.09], 10);
+        case 'aucun-habitant':
+            show_figure('france-aucun-habitant');
+            break;
+        case 'detail-1':
+            show_figure('aucun-habitant-detail');
+            break;
+        case 'detail-chart-1':
+            show_figure('chart-superficie-france');
             break;
         case '3':
             map.setView([51.505, -0.09], 5);
@@ -43,6 +65,14 @@ document.addEventListener('scroll-scene-exit', (event) => {
     const step = event.detail.element.getAttribute('step');
     event.detail.element.classList.remove('is-active');
     switch (step) {
+        case 'aucun-habitant':
+            if (!event.detail.isScrollingDown) {
+                hide_figure('france-aucun-habitant');
+            }
+            break;
+        case 'detail-1':
+            hide_figure('aucun-habitant-detail');
+            break;
         // case 'hide map':
         //     if (!event.detail.isScrollingDown) {
         //         document.querySelector('#map').removeAttribute('hidden')
