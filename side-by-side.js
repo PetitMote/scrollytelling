eb01079e6a4df8b601cd2ed196ce417cd76b00aa
Moté <automates@lepouete.fr>
2023-06-25 15:28:55 +0200

    Some changes…

--- side-by-side.js
+++ side-by-side.js
@@ -5,6 +5,21 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+
+const ctx = document.getElementById('chart');
+
+new Chart(ctx, {
+    type: 'pie',
+    data: {
+        labels: ['Aucun habitant', 'Présence d’habitants'],
+        datasets: [{
+            label: 'Pourcentage de la surface',
+            data: [40, 60],
+            borderWidth: 1
+        }]
+    }
+})
+
 document.addEventListener('scroll-scene-enter', (event) => {
     const step = event.detail.element.getAttribute('step');
     event.detail.element.classList.add('is-active');
@@ -18,9 +33,9 @@ document.addEventListener('scroll-scene-enter', (event) => {
         case '3':
             map.setView([51.505, -0.09], 5);
             break;
-        case 'hide map':
-            document.querySelector('#map').setAttribute('hidden', 'true')
-            break;
+        // case 'hide map':
+        //     document.querySelector('#map').setAttribute('hidden', 'true')
+        //     break;
     }
 })
 
@@ -28,10 +43,10 @@ document.addEventListener('scroll-scene-exit', (event) => {
     const step = event.detail.element.getAttribute('step');
     event.detail.element.classList.remove('is-active');
     switch (step) {
-        case 'hide map':
-            if (!event.detail.isScrollingDown) {
-                document.querySelector('#map').removeAttribute('hidden')
-            }
-            break;
+        // case 'hide map':
+        //     if (!event.detail.isScrollingDown) {
+        //         document.querySelector('#map').removeAttribute('hidden')
+        //     }
+        //     break;
     }
 })
